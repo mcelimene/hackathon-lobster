55bc6df61ea265284113211b43433b36f579e537
Vince <Vini8383@hotmail.com>
2018-06-02 17:00:54 +0200

    profil

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) { }
 
+/*  	goToProfil() {
+    	this.navCtrl.push('page-profil');
   }
+
+	goToRadar() {
+    	this.navCtrl.push('page-radar');
+  }  	
+
+	goToDemandes() {
+    	this.navCtrl.push('page-demandes');
+  }
+
+  	goToMessages() {
+    	this.navCtrl.push('page-messages');
+  }
+*/
 
 }
