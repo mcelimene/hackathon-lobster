af0b733fb964eb91f02106c793c6e5db290c0903
Magali <magali_thuaire@yahoo.fr>
2018-06-03 14:34:02 +0200

    fix#8 general

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
-import { AuthPage } from '../auth/auth';
 import { SigninPage } from '../signin/signin';
 import { ConversationsPage } from '../conversations/conversations';
 import { MessagesPage } from '../messages/messages';
@@ -22,10 +20,7 @@ export class TabsPage {
   tab3Root = ContactPage;
   tab4Root = SigninPage;
   tab5Root = AuthPage;
-  tab4Root = ConversationsPage;
-  tab5Root = MessagesPage;
   tab6Root = AuthPage;
-
 
   constructor() {
 
