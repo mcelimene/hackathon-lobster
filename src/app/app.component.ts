28dee744aa0cd46f38ab2edd46562df763d64599
Magali <magali_thuaire@yahoo.fr>
2018-06-03 16:55:08 +0200

    presque terminee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import * as firebase from 'firebase';
 // import { AddMemberPage } from '../pages/add-member/add-member';
-import { TabsPage } from '../pages/tabs/tabs';
-// import { AuthPage } from '../pages/auth/auth';
+// import { TabsPage } from '../pages/tabs/tabs';
+import { AuthPage } from '../pages/auth/auth';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage:any = AuthPage;
 
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
